2b050a51b5c7dc0bda2d98a24f2ce44123cb4880
WangKangkang <wangkangkang@51heyou.com>
2018-05-30 15:02:54 +0800

    Initial Commit

---
+++ FuncsTestProjectTests/FuncsTestProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  FuncsTestProjectTests.m
+//  FuncsTestProjectTests
+//
+//  Created by 刘云 on 30/5/18.
+//  Copyright © 2018年 王康康. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FuncsTestProjectTests : XCTestCase
+
+@end
+
+@implementation FuncsTestProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
